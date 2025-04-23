1ea83cd96cd098f2ba99268249fea66819e95258
ESTUDIANTE <ESTUDIANTE@SL404-17>
2025-04-23 09:48:00 -0500

    NUEVOS AVANCES Y MEJORAS EN EL DISEÑO DE INTERFAZ Y ICONO

--- enrollmentSoftware/src/application/Main.java
+++ enrollmentSoftware/src/application/Main.java
@@ -6,61 +6,80 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
-	private static VBox rootLayout;
-	private static Stage primaryStage;
+    private static VBox rootLayout;
+    private static Stage primaryStage;
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Main.primaryStage = primaryStage;
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Main.primaryStage = primaryStage;
 
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainMenu.fxml"));
-			rootLayout = loader.load();
+            
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainMenu.fxml"));
+            rootLayout = loader.load();
 
-			Scene scene = new Scene(rootLayout);
+            
+            Scene scene = new Scene(rootLayout);
+            scene.getStylesheets().add(getClass().getResource("/view/style.css").toExternalForm());
 
-	
-			scene.getStylesheets().add(getClass().getResource("/view/style.css").toExternalForm());
+            
+            loadIcon();
 
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Enrollment Software");
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Enrollment Software");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    private void loadIcon() {
+        try {
+            
+        	Image icon = new Image(getClass().getResourceAsStream("/application/icons/UDI.png"));
 
 
-	
-	public static void loadView(String fxmlFile) {
-		try {
-			FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlFile));
-			Parent view = loader.load();
-			rootLayout.getChildren().setAll(view); 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            if (icon.isError()) {
+                System.out.println("Error: No se pudo cargar el icono.");
+            } else {
+                primaryStage.getIcons().add(icon);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al cargar el icono: " + e.getMessage());
+        }
+    }
 
-	
-	public static void loadScene(String fxmlFile) {
-		try {
-			Parent root = FXMLLoader.load(Main.class.getResource(fxmlFile));
-			Scene scene = primaryStage.getScene();
-			if (scene == null) {
-				scene = new Scene(root, 600, 600);
-				primaryStage.setScene(scene);
-			} else {
-				scene.setRoot(root);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void loadView(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlFile));
+            Parent view = loader.load();
+            rootLayout.getChildren().setAll(view);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void loadScene(String fxmlFile) {
+        try {
+            Parent root = FXMLLoader.load(Main.class.getResource(fxmlFile));
+            Scene scene = primaryStage.getScene();
+            if (scene == null) {
+                scene = new Scene(root, 600, 600);
+                primaryStage.setScene(scene);
+            } else {
+                scene.setRoot(root);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
